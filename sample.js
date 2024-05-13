17d7518e6993639e15a2ada0e6bc5468cc2ad488
SHAZZAD-BS1572 <shazzad.mia@brainstation-23.com>
2024-05-13 18:30:44 +0600

    Add

--- sample.js
+++ sample.js
@@ -1,13 +1,14 @@
 import http from "k6/http";
-import { sleep } from "k6";
+import { check, sleep } from "k6";
 export const options = {
   vus: 10,
-  duration: "30s",
+  duration: "5s",
 };
 export default function () {
   const baseUrl = "https://reqres.in/";
   const endPoint = "api/users/2";
   const res = http.get(`${baseUrl}${endPoint}`);
-  console.log(res);
+  check(res, { "is status 200": (r) => r.status === 200 });
+  //   console.log(res);
   sleep(1);
 }
