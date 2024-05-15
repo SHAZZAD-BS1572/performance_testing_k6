f2c0edba269908c5b991dbc2f1908d0ba5dee98b
SHAZZAD-BS1572 <shazzad.mia@brainstation-23.com>
2024-05-15 10:26:00 +0600

    new10:25am

---
+++ scripts/metrics.js
@@ -0,0 +1,46 @@
+import http from "k6/http";
+import { check, sleep } from "k6";
+import exec from "k6/execution";
+import { Counter } from "k6/metrics";
+
+// const allRequests = new Counter("count_requests");
+const allError = new Counter("error_count");
+
+export const options = {
+  thresholds: {
+    error_count: [{ threshold: "count>=5", abortOnFail: true }], // Abort if errors are 5 or more
+  },
+  scenarios: {
+    smoke: {
+      executor: "ramping-vus",
+      startVUs: 0,
+      stages: [
+        { duration: "20s", target: 10 },
+        { duration: "30s", target: 10 },
+        { duration: "0s", target: 0 },
+      ],
+      gracefulRampDown: "30s",
+    },
+  },
+};
+
+export default function () {
+  console.info(
+    `Iterations id ==> ${exec.scenario.iterationInTest} VU id${exec.vu.idInTest}`
+  );
+
+  const baseUrl = "https://reqres.in/";
+  const endPoint = "api/users/2";
+  const res = http.get(`${baseUrl}${endPoint}`);
+  if (res.status >= 400) allError.add(1);
+  check(res, { "is status 200": (r) => r.status === 200 });
+
+  // allRequests.add(1);
+
+  const res2 = http.get(`https://reqres.in/api/unknown/23`);
+  if (res2.status >= 400) allError.add(1);
+
+  check(res, { "is status 200": (r) => r.status === 200 });
+
+  sleep(1);
+}
--- scripts/sample.js
+++ scripts/sample.js
@@ -3,40 +3,44 @@ import { check, sleep } from "k6";
 import exec from "k6/execution";
 export const users = JSON.parse(open("data.json"));
 export const options = {
-  scenarios: {
-    // accountCreate: {
-    //   executor: "per-vu-iterations",
-    //   vus: 5,
-    //   iterations: 10,
-    // },
-    accountCreate2: {
-      executor: "shared-iterations",
-      vus: 3,
-      iterations: 500,
-    },
-
-    // accountCreate3: {
-    //   executor: "constant-vus",
-    //   vus: 10,
-    //   duration: "120s",
-    //iterations: 10,
-    //},
-
-    // accountCreate4: {
-    //   executor: "ramping-vus",
-    //   startVUs: 0,
-    //   stages: [
-    //     { duration: "20s", target: 10 },
-    //     { duration: "50s", target: 100 },
-    //     { duration: "20s", target: 5 },
-    //   ],
-    //   gracefulRampDown: "30s",
-    // },
+  thresholds: {
+    http_req_duration: [
+      { threshold: "p(90)<35", abortOnFail: true, delayAbortEval: "10s" }, // 90% of requests should be below 35ms, abort test if this fails
+    ],
   },
 
-  // vus: 10,
-  // iterations: 5,
-  // duration: "5m",
+  scenarios: {
+    accountCreate: {
+      executor: "per-vu-iterations",
+      vus: 5,
+      iterations: 10,
+    },
+    accountCreate2: {
+      executor: "shared-iterations",
+      vus: 1,
+      iterations: 1,
+    },
+
+    accountCreate3: {
+      executor: "constant-vus",
+      vus: 10,
+      duration: "120s",
+      iterations: 10,
+    },
+    accountCreate4: {
+      executor: "ramping-vus",
+      startVUs: 0,
+      stages: [
+        { duration: "20s", target: 100 },
+        { duration: "30s", target: 100 },
+        { duration: "20s", target: 0 },
+      ],
+      gracefulRampDown: "30s",
+    },
+  },
+  vus: 10,
+  iterations: 10,
+  duration: "5m",
 };
 export default function () {
   console.info(
@@ -45,18 +49,17 @@ export default function () {
     } Data id ${(exec.scenario.iterationInTest % users.length) + 1}`
   );
 
-  // console.info(
-  //   `Iterations id ==> ${exec.scenario.iterationInTest} VU id${exec.vu.idInTest}`
-  // );
+  console.info(
+    `Iterations id ==> ${exec.scenario.iterationInTest} VU id${exec.vu.idInTest}`
+  );
 
-  // console.log(exec.vu.idInTest);
+  console.log(exec.vu.idInTest);
 
   const baseUrl = "https://reqres.in/";
   const endPoint = "api/users/2";
   const res = http.get(`${baseUrl}${endPoint}`);
   check(res, { "is status 200": (r) => r.status === 200 });
   res.body;
-  //   console.log(res);
+  console.log(res);
   sleep(1);
 }
-//new
