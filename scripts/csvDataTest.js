50f2fb3ef9671bd94efef19ba5dafa684540a381
SHAZZAD-BS1572 <shazzad.mia@brainstation-23.com>
2024-05-16 18:19:39 +0600

    6:19pm

--- scripts/csvDataTest.js
+++ scripts/csvDataTest.js
@@ -1,24 +1,41 @@
 import papaparse from "https://jslib.k6.io/papaparse/5.1.1/index.js";
 import { SharedArray } from "k6/data";
 import exec from "k6/execution";
+import http from "k6/http";
+import { check, sleep } from "k6";
 
 const csvData = papaparse.parse(open("/scripts/csvData.csv"), {
   header: true,
 }).data;
 
 export const options = {
-  scenarios: {
-    loginWithCSV: {
-      executor: "shared-iterations",
-      vus: 1,
-      iterations: csvData.length,
-    },
-  },
+  stages: [
+    { duration: "2m", target: 2000 }, // fast ramp-up to a high point
+    // No plateau
+    { duration: "1m", target: 0 }, // quick ramp-down to 0 users
+  ],
+  //   scenarios: {
+  //     loginWithCSV: {
+  //       executor: "shared-iterations",
+  //       vus: 1,
+  //       iterations: csvData.length,
+  //     },
+  //   },
 };
 
 export default function (data) {
   // 3. VU code
-  const user = csvData[exec.scenario.iterationInTest];
+  //   const user = csvData[exec.scenario.iterationInTest];
+  const res = http.get(
+    `https://reqres.in/api/users/${exec.scenario.iterationInTest}`
+  );
+  check(res, { "is status 200": (r) => r.status === 200 });
+
   // console.info(exec.scenario.iterationInTest + "====>" + exec.vu.idInTest);
-  console.log(user.email);
+  //   const resData = res.json("data");
+  //   console.log(resData.email);
+  //   console.log(user.email);
+  //   check(res, {
+  //     "is user email correct": () => resData.email === user.email,
+  //   });
 }
--- scripts/dist/csvDataTest.dev.js
+++ scripts/dist/csvDataTest.dev.js
@@ -12,6 +12,10 @@ var _data = require("k6/data");
 
 var _execution = _interopRequireDefault(require("k6/execution"));
 
+var _http = _interopRequireDefault(require("k6/http"));
+
+var _k = require("k6");
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
 
 var csvData = _index["default"].parse(open("/scripts/csvData.csv"), {
@@ -19,19 +23,40 @@ var csvData = _index["default"].parse(open("/scripts/csvData.csv"), {
 }).data;
 
 var options = {
-  scenarios: {
-    loginWithCSV: {
-      executor: "shared-iterations",
-      vus: 1,
-      iterations: csvData.length
-    }
-  }
+  stages: [{
+    duration: "2m",
+    target: 2000
+  }, // fast ramp-up to a high point
+  // No plateau
+  {
+    duration: "1m",
+    target: 0
+  } // quick ramp-down to 0 users
+  ] //   scenarios: {
+  //     loginWithCSV: {
+  //       executor: "shared-iterations",
+  //       vus: 1,
+  //       iterations: csvData.length,
+  //     },
+  //   },
+
 };
 exports.options = options;
 
 function _default(data) {
   // 3. VU code
-  var user = csvData[_execution["default"].scenario.iterationInTest]; // console.info(exec.scenario.iterationInTest + "====>" + exec.vu.idInTest);
+  //   const user = csvData[exec.scenario.iterationInTest];
+  var res = _http["default"].get("https://reqres.in/api/users/".concat(_execution["default"].scenario.iterationInTest));
 
-  console.log(user.email);
+  (0, _k.check)(res, {
+    "is status 200": function isStatus200(r) {
+      return r.status === 200;
+    }
+  }); // console.info(exec.scenario.iterationInTest + "====>" + exec.vu.idInTest);
+  //   const resData = res.json("data");
+  //   console.log(resData.email);
+  //   console.log(user.email);
+  //   check(res, {
+  //     "is user email correct": () => resData.email === user.email,
+  //   });
 }
